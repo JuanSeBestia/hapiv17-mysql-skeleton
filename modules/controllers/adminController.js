e6959ab58b0db01b8a9af9f6ea6e265494d5aaaa
Jess Casas <jlcasasg@inkrementaldev.co>
2019-03-15 21:46:13 -0500

    change table

--- modules/controllers/adminController.js
+++ modules/controllers/adminController.js
@@ -1,22 +1,21 @@
-
 const Boom = require('boom')
 
 const Lib = require('../libs/commFunctions')
 const dbQuery = require('../databaseInteractions')
 const api_reference = "Admin_Controller"
 
-module.exports.login = async function(creds) {
+module.exports.login = async function (creds) {
     const loggerData = {
         api_reference: api_reference,
         event: "/admin/login",
         body: creds
     }
     winstonLogger.info(loggerData)
-    try {      
-        let sql = `SELECT * FROM tb_admins WHERE email = ?`
-        let params = [creds.email]     
+    try {
+        let sql = `SELECT * FROM Users WHERE email = ?`
+        let params = [creds.email]
         let adminInfo = await dbQuery.executeSQLQuery(sql, params)
-        if(!adminInfo.length){
+        if (!adminInfo.length) {
             throw Boom.unauthorized(Constant.errorMessages.invalidCreds)
         }
         let password = adminInfo[0].password
@@ -24,18 +23,12 @@ module.exports.login = async function(creds) {
         const dataToSign = {
             id: adminInfo[0].admin_id,
             role: 'Admin',
-            creationTime: new Date(), 
+            creationTime: new Date(),
         }
         const expireTime = '1d'
         const accessToken = await Lib.generateToken(dataToSign, expireTime);
 
-        let data = {
-            id: adminInfo[0].admin_id,
-            firstName: adminInfo[0].first_name,
-            lastName: adminInfo[0].last_name,
-            email: adminInfo[0].email,
-            accessToken: accessToken
-        };
+        let data = adminInfo[0];
         return Lib.successResponse(data);
     } catch (error) {
         if (error.isBoom) {
@@ -49,18 +42,18 @@ module.exports.login = async function(creds) {
     }
 }
 
-module.exports.accessTokenLogin = async function(accessToken, adminData){
+module.exports.accessTokenLogin = async function (accessToken, adminData) {
     const loggerData = {
         api_reference: api_reference,
         event: "/admin/access_token_login",
         body: adminData
     }
     winstonLogger.info(loggerData)
-    try {      
+    try {
         let sql = `SELECT * FROM tb_admins WHERE admin_id = ?`
-        let params = [adminData.id]     
+        let params = [adminData.id]
         let adminInfo = await dbQuery.executeSQLQuery(sql, params)
-        if(!adminInfo.length){
+        if (!adminInfo.length) {
             throw Boom.unauthorized(Constant.errorMessages.invalidCreds)
         }
 
